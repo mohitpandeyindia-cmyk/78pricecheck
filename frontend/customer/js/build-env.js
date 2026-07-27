d6df4be0bd4d15f79678533cb42ecf90a3abe7b5
78 Deployer <deployer@78supermaart.com>
2026-07-27 07:07:09 +0530

    Polish scanner layout alignment and background

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202607270122",
-  commit: "5795420",
+  build: "202607270136",
+  commit: "3c49ade",
   branch: "main",
-  buildTime: "2026-07-27T01:22:50.461Z",
+  buildTime: "2026-07-27T01:36:42.217Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
