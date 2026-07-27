92ce36047314135a3243b1310cf646d92f3784c2
78 Deployer <deployer@78supermaart.com>
2026-07-27 15:03:29 +0530

    Fix scanner page layout architecture and align content cards

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202607270205",
-  commit: "3a49fb7",
+  build: "202607270933",
+  commit: "e314ff9",
   branch: "main",
-  buildTime: "2026-07-27T02:05:23.872Z",
+  buildTime: "2026-07-27T09:33:03.145Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
--- frontend/customer/sw.js
+++ frontend/customer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '78pricecheck-202607270205';
+const CACHE_NAME = '78pricecheck-202607270933';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
