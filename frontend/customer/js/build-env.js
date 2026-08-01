2ff1e4bb7def42d31b2fab130fa3d102f4e99f8e
78 Deployer <deployer@78supermaart.com>
2026-08-02 03:22:20 +0530

    Deploy: Section 3 V5.2 single primary surface & Band 2 optical vertical centering (Build 202608012151)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608012128",
-  commit: "603c4d6",
+  build: "202608012151",
+  commit: "94e7707",
   branch: "main",
-  buildTime: "2026-08-01T21:28:37.723Z",
+  buildTime: "2026-08-01T21:51:54.212Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
--- frontend/customer/sw.js
+++ frontend/customer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '78pricecheck-202608012128';
+const CACHE_NAME = '78pricecheck-202608012151';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
