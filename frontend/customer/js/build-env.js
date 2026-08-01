94e7707e1a8f0f88f638f9f5f1c96688603eca2d
78 Deployer <deployer@78supermaart.com>
2026-08-02 02:59:14 +0530

    Deploy: Section 3 V5.1 optical hierarchy & premium surface refinement (+20% name, +15-20% price, 3-layer white material) (Build 202608012128)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608012051",
-  commit: "089140b",
+  build: "202608012128",
+  commit: "603c4d6",
   branch: "main",
-  buildTime: "2026-08-01T20:51:34.369Z",
+  buildTime: "2026-08-01T21:28:37.723Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
--- frontend/customer/sw.js
+++ frontend/customer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '78pricecheck-202608012051';
+const CACHE_NAME = '78pricecheck-202608012128';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
