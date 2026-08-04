6fd4a90380c381cc612e75d9b4ab86abaab3f81e
78 Deployer <deployer@78supermaart.com>
2026-08-05 00:29:03 +0530

    Deploy: Hot Deals Carousel V2.8 - Static Visual Layout Calibration (nth-child transform-origins: left, center, right for exact 0px left & viewport width right boundary alignment) (Build 202608041858)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608041856",
-  commit: "dafd40e",
+  build: "202608041858",
+  commit: "415e26a",
   branch: "main",
-  buildTime: "2026-08-04T18:56:30.748Z",
+  buildTime: "2026-08-04T18:58:45.309Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
