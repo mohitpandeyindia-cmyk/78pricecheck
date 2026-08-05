a59962a2744c78b1fe026271660fb8bd9f9a4e50
78 Deployer <deployer@78supermaart.com>
2026-08-05 22:50:55 +0530

    Deploy: Updated Layer 1 Night Background assets (night_1 through night_9) with futuristic night skyline image (Build 202608051720)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608051605",
-  commit: "dcf8e30",
+  build: "202608051720",
+  commit: "3c0a8c4",
   branch: "main",
-  buildTime: "2026-08-05T16:05:06.000Z",
+  buildTime: "2026-08-05T17:20:12.381Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
