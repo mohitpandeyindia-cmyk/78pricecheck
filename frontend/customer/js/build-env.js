dc7e49b97bbc0c2f92d5ffdfc63bfb9d30af5c28
78 Deployer <deployer@78supermaart.com>
2026-08-14 20:37:03 +0530

    Fix: Shift sale price compartment right by 3% to left: 17.73% (Build 202608141506)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608141458",
-  commit: "2c791c7",
+  build: "202608141506",
+  commit: "220f6fa",
   branch: "main",
-  buildTime: "2026-08-14T14:58:04.121Z",
+  buildTime: "2026-08-14T15:06:29.750Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
--- frontend/customer/sw.js
+++ frontend/customer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '78pricecheck-202608141458';
+const CACHE_NAME = '78pricecheck-202608141506';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
