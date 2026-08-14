a8b8e86ba25305aef9cbb4379a85ce4589d4ee7c
78 Deployer <deployer@78supermaart.com>
2026-08-14 22:43:10 +0530

    Fix: Replace wholesale template with latest user image artwork (Build 202608141712)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608141700",
-  commit: "75372b8",
+  build: "202608141712",
+  commit: "ae2398e",
   branch: "main",
-  buildTime: "2026-08-14T17:00:10.761Z",
+  buildTime: "2026-08-14T17:12:05.544Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
--- frontend/customer/sw.js
+++ frontend/customer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '78pricecheck-202608141700';
+const CACHE_NAME = '78pricecheck-202608141712';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
