2403413387e4005825badc8cbf6fa868a71f9e77
78 Deployer <deployer@78supermaart.com>
2026-08-15 00:16:30 +0530

    Fix: Replace OFF percentage template with latest user image artwork (Build 202608141845)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608141729",
-  commit: "85a2a5c",
+  build: "202608141845",
+  commit: "2408063",
   branch: "main",
-  buildTime: "2026-08-14T17:29:03.878Z",
+  buildTime: "2026-08-14T18:45:00.265Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
