97bffd813e2336f7f1f7bfacd6ed027798c04d39
78 Deployer <deployer@78supermaart.com>
2026-08-15 19:47:26 +0530

    Update: Replace template PNG graphics with latest user uploaded template artwork (Build 202608151416)

--- frontend/customer/js/build-env.js
+++ frontend/customer/js/build-env.js
@@ -1,10 +1,10 @@
 // Automatically generated build metadata
 window.APP_BUILD = {
   version: "0.1.0",
-  build: "202608151315",
-  commit: "2403413",
+  build: "202608151416",
+  commit: "fed7ad0",
   branch: "main",
-  buildTime: "2026-08-15T13:15:06.170Z",
+  buildTime: "2026-08-15T14:16:47.927Z",
   environment: "production",
   serviceWorkerEnabled: true
 };
